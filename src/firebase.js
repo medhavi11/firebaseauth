dd7a474050cce6a5598961ba7f7022162990ef68
Medhavi Basera <1905781@kiit.ac.in>
2021-07-29 16:37:10 +0530

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -2,3 +2,18 @@
 // git remote add origin https://github.com/medhavi11/xyz.git
 // git branch -M main
 // git push -u origin main
+
+import firebase from "firebase";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID,
+});
+//export a varaible for authentication
+export const auth = app.auth();
+export default app;
